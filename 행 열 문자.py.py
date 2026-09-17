61c17da9d81085aa4ba9907eb6ed81a20acbc220
ey_2673 <liahlove1004@gmail.com>
2026-09-17 18:34:59 +0900

    Delete 행 열 문자.py.py

--- 행 열 문자.py.py
+++
@@ -1,9 +0,0 @@
-#행 열 문자 인수로 받아 중첩 반복문으로 출력
-
-def printPattern(rows=5, cols=5, char="*"):
-    for _ in range(rows):
-        for _ in range(cols):
-            print(char,end="")
-        print()
-        
-printPattern(3,10,"%")
